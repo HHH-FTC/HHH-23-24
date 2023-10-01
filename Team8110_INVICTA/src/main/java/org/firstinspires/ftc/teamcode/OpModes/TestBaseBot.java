c5fc6f3789940c3ac1dccf1948f2120f084491b9
Zach <admiralzacbar2@gmail.com>
2023-09-30 22:38:41 -0400

    test bot but with speed controls

--- Team8110_INVICTA/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestBaseBot.java
+++ Team8110_INVICTA/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestBaseBot.java
@@ -1,26 +1,36 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Chassis.StraferChassisBase;
 
-@TeleOp
-public class TestBaseBot extends LinearOpMode {
+import ftc.rogue.blacksmith.BlackOp;
+import ftc.rogue.blacksmith.Scheduler;
+import ftc.rogue.blacksmith.annotations.CreateOnGo;
+import ftc.rogue.blacksmith.listeners.ReforgedGamepad;
 
+@TeleOp(name="SpeedControls")
+public class TestBaseBot extends BlackOp {
+    @CreateOnGo(passHwMap = true)
+    StraferChassisBase base;
     @Override
-    public void runOpMode() {
-        StraferChassisBase base = new StraferChassisBase(hardwareMap);
+    public void go() {
+        ReforgedGamepad gp = getReforgedGamepad1();
+        MultipleTelemetry tm = mTelemetry();
 
-        waitForStart();
+        Pose2d loc = new Pose2d(gp.left_stick_y.get(), -gp.right_stick_x.get(), -gp.left_stick_x.get());
+        Pose2d halfloc = new Pose2d(loc.getX()/2, loc.getY()/2, loc.getY()/2);
 
-        while (opModeIsActive()) {
-            Pose2d loc = new Pose2d(gamepad1.left_stick_y, -gamepad1.right_stick_x, -gamepad1.left_stick_x);
-            base.setDrivePower(loc);
+        String mode = gp.right_bumper.get() ? "Fast" : "Slow";
+        gp.right_bumper.whileTrue(() -> base.setWeightedDrivePower(loc));
+        gp.right_bumper.whileFalse(() -> base.setWeightedDrivePower(halfloc));
 
-            telemetry.addData("Positions:", loc);
-            telemetry.update();
-        }
+        Scheduler.launchOnStart(this, () -> {
+            tm.addData("Mode", mode);
+            tm.addData("Fast Pos", loc);
+            tm.addData("Slow Pos", loc);
+        });
     }
 }
